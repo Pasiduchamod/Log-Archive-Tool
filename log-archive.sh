7a102ac5b2c22336e261164795ab617d51ed9ef1
pasiduchamod <jayaweera.pasindu123@gmail.com>
2025-12-18 18:59:46 +0530

    Add cleanup of archived logs older than 14 days before archiving

--- log-archive.sh
+++ log-archive.sh
@@ -7,11 +7,25 @@ if [ -z "$LOG_DIR" ]; then
 	exit 1
 fi
 
+if [ ! -d "$LOG_DIR" ]; then
+	echo "Error : $LOG_DIR is not exists."
+	exit 1
+fi
+
+if [ ! "$(ls -A "$LOG_DIR")" ]; then
+	echo "Directory $LOG_DIR is empty. Nothing to archive."
+	exit 1
+fi
+
 ARCHIVE_DIR="$HOME/log_archives"
 mkdir -p "$ARCHIVE_DIR"
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 ARCHIVE_NAME="logs_archive_${TIMESTAMP}.tar.gz"
+
+DAYS=14
+
+find "$ARCHIVE_DIR" -type f -name "*.tar.gz" -mtime +$DAYS -delete
 
 tar -czf "$ARCHIVE_DIR/$ARCHIVE_NAME" -C "$LOG_DIR" .
 
